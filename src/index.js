4b4cdeffd9b875d2084925957945cdcb19ad0e40
Steven Washington <washington.steven@gmail.com>
2019-02-07 21:31:41 -0500

    removing redundant init

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from "./serviceWorker";
 import netlifyIdentity from "netlify-identity-widget";
 
 window.netlifyIdentity = netlifyIdentity;
-netlifyIdentity.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
